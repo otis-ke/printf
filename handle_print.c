af0ee86a697f3915883844c5b475e4d7b3e4ed30
otis-ke <vincentotiskisia.com>
2023-01-26 16:21:31 -0800

    error

--- handle_print.c
+++ handle_print.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
@@ -19,7 +19,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'X', print_hexa_upper}, {'p', write_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
--- write_handlers.c
+++ write_handlers.c
@@ -124,8 +124,8 @@ int write_num(int ind, char buffer[],
 
 	}
 	if (extra_c)
-		buffer[--ind] = extra_c
-	return (write(1, &buffer[ind], length));
+		buffer[--ind] = extra_c;
+			return (write(1, &buffer[ind], length));
 }
 /**
  * write_unsgnd - Writes an unsigned number
